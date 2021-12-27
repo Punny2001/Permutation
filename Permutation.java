d065fac046a8514461bc476e021c9a8cfa00dfc2
Punny <70284846+linezeek@users.noreply.github.com>
2021-12-27 22:27:23 +0700

    Initial commit

---
+++ Permutation.java
@@ -0,0 +1,36 @@
+// Krissanapong Palakham    Sec:1   6288102
+public class Permutation {
+	
+	public static void main(String[] args) 
+    { 
+        String str = "ABC"; 
+        int n = str.length(); 
+        System.out.println("The string of permutation of "+str+","+n);
+        Permutation permutation = new Permutation(); 
+        permutation.permute(str, 0, n-1); 
+    } 
+	
+	// l is starting index, r is end index
+	private void permute(String str, int l, int r) 
+    { 
+        if (l == r) System.out.println(str); 
+        else { 
+            for (int i = l; i <= r; i++) { 
+                str = swap(str, l, i); 
+                permute(str, l+1, r); 
+                str = swap(str, l, i); 
+            } 
+        } 
+    } 
+	
+	public String swap(String a, int i, int j) 
+    { 
+        char temp; 
+        char[] charArray = a.toCharArray(); 
+        temp = charArray[i]; 
+        charArray[i] = charArray[j]; 
+        charArray[j] = temp; 
+        return String.valueOf(charArray); 
+    } 
+	
+}
